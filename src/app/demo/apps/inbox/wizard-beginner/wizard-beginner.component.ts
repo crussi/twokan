4e6ea0b1312a04241c634fa4cc07d17de8f0a8aa
crussi <christopher.russi@pacificlife.net>
2017-05-04 16:28:08 -0700

    wizard beginner wip

--- src/app/demo/apps/inbox/wizard-beginner/wizard-beginner.component.ts
+++ src/app/demo/apps/inbox/wizard-beginner/wizard-beginner.component.ts
@@ -15,7 +15,7 @@ export class WizardBeginnerComponent implements OnInit {
 
   selectedIndex: number = 0;
   notActionable: string;
-
+  isproject: boolean = false;
   constructor() { }
 
   ngOnInit() {
@@ -25,11 +25,70 @@ export class WizardBeginnerComponent implements OnInit {
     //this.selectedIndex += 1;
     console.log('notActionable: ' + this.notActionable);
     console.log('nextStep: ' + step);
+    switch (step) {
+      case 'isactionable':
+        this.selectedIndex = 0;
+        break;
+      case 'multiplesteps':
+        this.selectedIndex = 1;
+        break;   
+      case 'notactionable':
+        this.selectedIndex = 2;
+        break;     
+      case 'confirmlist':
+        this.selectedIndex = 3;
+        break;           
+      case 'process-notactionable':  
+        console.log('inbox item processed ...');      
+        break;
+      case 'isproject':
+        this.isproject = true;  
+        this.selectedIndex = 4;      
+        break;
+      case 'notproject':
+        this.isproject = false;  
+        this.selectedIndex = 6;      
+        break;
+      case 'addactions':  
+        this.selectedIndex = 5;    
+        break;
+      case 'donein2':  
+        this.selectedIndex = 7;    
+        break;  
+      case 'doitnow':  
+        this.selectedIndex = 8;    
+        break;    
+      case 'candelegate':  
+        this.selectedIndex = 8;    
+        break;  
+      case 'deleate':  
+        this.selectedIndex = 8;    
+        break;                               
+      default:
+        break;
+    }
   }
-  previousStep(step) {
-    //this.selectedIndex -= 1;
-    console.log('previousStep: ' + step);
-  }
+  // previousStep(step) {
+  //   //this.selectedIndex -= 1;
+  //   console.log('previousStep: ' + step);
+  //   switch (step) {
+  //     case 'notactionable':
+  //       this.selectedIndex = 2;
+  //       break;
+  //     case 'multiplesteps':
+  //       this.selectedIndex = 1;
+  //       break;
+  //     case 'isactionable':
+  //       this.selectedIndex = 0;
+  //       break;
+  //     case 'notproject':
+  //       this.isproject = false;  
+  //       this.selectedIndex = 6;      
+  //       break;                
+  //     default:
+  //       break;
+  //   }    
+  // }
   isActionable(yesno){
     console.log('isActionable: ' + yesno);
     if (yesno.toLowerCase() == 'yes') {
@@ -40,14 +99,14 @@ export class WizardBeginnerComponent implements OnInit {
       this.selectedIndex = 2;
     }
   }
-  isProject(yesno){
-    console.log('isProject: ' + yesno);
-    if (yesno.toLowerCase() == 'yes') {
-      //this.selectedIndex = 1;
-    } else {
-      //this.selectedIndex = 2;
-    }
-  }  
+  // isProject(yesno){
+  //   console.log('isProject: ' + yesno);
+  //   if (yesno.toLowerCase() == 'yes') {
+  //     this.selectedIndex = 4;
+  //   } else {
+  //     this.selectedIndex = 2;
+  //   }
+  // }  
 
 
 
